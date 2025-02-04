f0ca8255f85ed5d215e10ca6f328b4c6f931892a
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-02-04 08:34:06 +0530

    Create 1800.java

---
+++ 1800.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max=nums[0];
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                sum+=nums[i];
+            }
+            else
+            {
+                max=Math.max(max,sum);
+                sum=nums[i];
+            }
+        }
+        max=Math.max(sum,max);
+        return max;
+    }
+}
