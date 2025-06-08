67a487cdbce9c2fcf94d4cadd58f4f043c159342
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-06-08 12:25:45 +0530

    Create 386.java

---
+++ 386.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>(n);
+        int curr = 1;
+
+        for (int i = 0; i < n; i++) {
+            result.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else {
+                while (curr % 10 == 9 || curr + 1 > n) {
+                    curr /= 10;
+                }
+                curr++;
+            }
+        }
+
+        return result;
+    }
+}
