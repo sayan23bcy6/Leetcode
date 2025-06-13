fad5f1634ebfe43bffcf51370dd5586c85f80351
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-06-13 13:00:29 +0530

    Create 2616.java

---
+++ 2616.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int left = 0, right = nums[nums.length - 1] - nums[0];
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (canFormPairs(nums, p, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean canFormPairs(int[] nums, int p, int maxDiff) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1 && count < p; i++) {
+            if (nums[i + 1] - nums[i] <= maxDiff) {
+                count++;
+                i++;
+            }
+        }
+        return count >= p;
+    }
+}
