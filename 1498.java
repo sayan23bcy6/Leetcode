ef80272dcde86fa6178438941c7217101eb8c106
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-06-29 12:31:19 +0530

    Create 1498.java

---
+++ 1498.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int MOD = 1_000_000_007;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] pow2 = new int[n];
+        pow2[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int left = 0, right = n - 1;
+        int result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + pow2[right - left]) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
