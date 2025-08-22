9f0c32aef850198973bd69810b5d4948b497c020
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-08-22 18:34:10 +0530

    Create 3195.java

---
+++ 3195.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int minRow = m, maxRow = -1;
+        int minCol = n, maxCol = -1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        if (maxRow == -1) return 0;
+
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+}
