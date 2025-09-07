1a0b5946b6e01001cff9a5ba740d576c07e99a2e
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-09-07 11:29:15 +0530

    Update 50.java

--- 50.java
+++ 50.java
@@ -9,14 +9,15 @@ class Solution {
     }
 
     private double fastPow(double x, long n) {
-        if (n == 0) return 1.0;
-        
-        double half = fastPow(x, n / 2);
-        
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
+        if((n & 1)==0)
+        {if(n==0)
+        return 1;
+            return fastPow(x*x,n/2);
+        }
+        else
+        {   if(n==1)
+            return x;
+            return x*fastPow(x,n-1);
         }
     }
 }
