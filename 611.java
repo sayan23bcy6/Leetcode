1279abbf760faff3e830800bbe5e75ece2ba9318
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-09-26 10:11:55 +0530

    Create 611.java

---
+++ 611.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] > nums[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
