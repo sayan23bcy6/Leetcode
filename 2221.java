934c6c006c76ee0fe2e64e284cbd8886650d63cb
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-09-30 09:48:04 +0530

    Create 2221.java

---
+++ 2221.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+       int n=nums.length;
+       if(n==1)
+       return nums[0];
+       while(n>0)
+       {
+        for(int i=0;i<n-1;i++)
+        {
+            nums[i]=(nums[i]+nums[i+1])%10;
+        }
+        n--;
+       }
+       return nums[0];
+    }
+}
