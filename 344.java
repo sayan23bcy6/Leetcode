d63fde0bdbdc7a93a62073844205fa9218bbada0
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-10-20 18:36:08 +0530

    Create 344.java

---
+++ 344.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int end=s.length-1;
+        int start=0;
+        while(start<end)
+        {
+            char temp=s[end];
+            s[end]=s[start];
+            s[start]=temp;
+            start++;
+            end--;
+        }
+    }
+}
