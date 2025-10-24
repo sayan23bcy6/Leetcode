31a7fe01e214e4ae8e1d1852c99643f1d41e5382
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2025-10-24 21:56:18 +0530

    Create 155.java

---
+++ 155.java
@@ -0,0 +1,31 @@
+class MinStack {
+    private List<Integer> stack;
+    private List<Integer> minStack;
+
+    public MinStack() {
+        stack = new ArrayList<>();
+        minStack = new ArrayList<>();
+    }
+
+    public void push(int val) {
+        stack.add(val);
+        if (minStack.isEmpty() || val <= minStack.get(minStack.size() - 1)) {
+            minStack.add(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.remove(stack.size() - 1);
+        if (removed == minStack.get(minStack.size() - 1)) {
+            minStack.remove(minStack.size() - 1);
+        }
+    }
+
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public int getMin() {
+        return minStack.get(minStack.size() - 1);
+    }
+}
