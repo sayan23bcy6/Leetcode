98967aa0c45ca09db2194e3a7dee1ed903ba7da8
Sayan Sinha <sayan23bcy6@iiitkottayam.ac.in>
2026-02-03 09:02:39 +0530

    Create 3637.java

---
+++ 3637.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if (n < 4) return false;
+
+        int i = 1;
+
+        while (i < n && nums[i] > nums[i - 1]) i++;
+        if (i == 1 || i == n) return false;
+
+        while (i < n && nums[i] < nums[i - 1]) i++;
+        if (i == n) return false;
+
+        while (i < n && nums[i] > nums[i - 1]) i++;
+
+        return i == n;
+    }
+}
